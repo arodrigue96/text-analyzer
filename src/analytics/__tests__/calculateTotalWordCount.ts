162a795012b566a4744c6df50c63933973f31bd6
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-02 20:04:28 +0200

    Add test verifications for calculateTotalWordCount function

---
+++ src/analytics/__tests__/calculateTotalWordCount.ts
@@ -0,0 +1,11 @@
+import { calculateTotalWordCount } from "../index.js";
+
+const text = "";
+const expectedWords = 0;
+const totalWords = calculateTotalWordCount(text);
+
+if (totalWords === expectedWords) {
+  console.log("Test Passed");
+} else {
+  console.log("Test Failed");
+}
