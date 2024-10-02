7d090adb1e6d90d09a8d06ce313b9abe363d146f
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-02 20:21:31 +0200

    Show the total number of words in the user interface

--- src/ui/analysis.ts
+++ src/ui/analysis.ts
@@ -1,4 +1,5 @@
 import { calculateParagraphsTotal } from "../analytics/index.js";
+import { calculateTotalWordCount } from "../analytics/index.js";
 
 const totalsContainer = document.querySelector(".totals");
 const shortWordsContainer = document.querySelector(".short");
@@ -125,4 +126,5 @@ export const analyzeText = (text: string): void => {
   // Implement the rest of the analysis here
   const paragraphsTotal = calculateParagraphsTotal(text);
   renderParagraphsTotal(paragraphsTotal);
+  renderWordsTotal(calculateTotalWordCount(text));
 };
