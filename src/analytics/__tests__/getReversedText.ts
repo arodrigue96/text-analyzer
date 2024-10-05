81a7c1d1b8dde8672c1c884dbfbb1c81590e2f4f
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-05 12:32:03 +0200

    Add test case for getReversedText function

---
+++ src/analytics/__tests__/getReversedText.ts
@@ -0,0 +1,12 @@
+import exp from "constants";
+import { getReversedText } from "../index.js";
+
+const text = "Hola como te llamas";
+const expectedReversedText = "llamas te como Hola";
+const reversedText = getReversedText(text);
+
+if (reversedText === expectedReversedText) {
+  console.log("Test passed");
+} else {
+  console.log("Test failed");
+}
